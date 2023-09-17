4fcb561b0f314577058bd5986ea522a5c01e60b4
Elia Gonzales Castro <140271809+EliLuxurious@users.noreply.github.com>
2023-09-17 17:05:58 -0500

    cambio

--- Categoria.py
+++ Categoria.py
@@ -2,19 +2,12 @@ import random
 import string
 from Libro import Libro
 
-class Categoria():
+class Categoria(Libro):
     cod_categoria = ''
     categoria = ''
 
     def __init__(self, cod_categoria, categoria):
         self.cod_categoria = cod_categoria
-        self.categoria = categoria
-
-    def set_cod_categoria(self, cod_categoria):
-        self.cod_categoria = cod_categoria
-
-    def get_cod_categoria(self):
-        return self.cod_categoria
     
     def generar_codigo_categoria(self, length=3):
         # Genera un código aleatorio de números
@@ -22,6 +15,12 @@ class Categoria():
         codigo_categoria = ''.join(random.choice(characters) for _ in range(length))
         self.cod_categoria = codigo_categoria
     
+    def get_cod_categoria(self):
+        return self.cod_categoria
+    
+    def set_cod_categoria(self, cod_categoria):
+        self.cod_categoria = cod_categoria
+        
     def asignar_libro(self, libro):
         self.libro = libro
     
@@ -29,4 +28,4 @@ class Categoria():
         return self.libro
     
     def reporte(self):
-        return f"Reporte del libro: {self.libro}\nCodigo de la categoria: {self.cod_categoria}\nCategoria: {self.categoria}"
+        return f"Reporte del libro: {self.libro}\nCodigo: {self.cod_categoria}"
