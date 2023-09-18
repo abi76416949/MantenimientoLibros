1877f5ec834abc1fb60f90a913fc0bfd5cc17770
Abigail Durand Palacios <127162244+abi76416949@users.noreply.github.com>
2023-09-18 00:58:11 -0500

    main console

--- Mantenimiento.py
+++ Mantenimiento.py
@@ -13,26 +13,26 @@ class Mantenimiento():
 ######################GENERAR UN CODIGO ALEATORIO PARA LIBRO Y CATEGORIA####################
     def generar_codigo_libro(self):
         #generamos un codigo de libro único asegurandonos que no se repita
-        codigo_libro = f'L{self.libro_counter: 04}'
+        codigo_libro = f'L{self.libro_counter: 004}'
         self.libro_counter += 1
         return codigo_libro 
 
     def generar_codigo_categoria(self):
         #generamos un codigo de categoria único asegurandonos que no se repita
-        codigo_categoria = f'C{self.libro_categoria_counter: 04}'
+        codigo_categoria = f'C{self.libro_categoria_counter: 004}'
         self.libro_categoria_counter += 1
         return codigo_categoria
     
     def generar_codigo_autor(self):
         #generamos un codigo de autor único asegurandonos que no se repita
-        codigo_autor = f'A{self.autor_counter: 04}'
+        codigo_autor = f'A{self.autor_counter: 004}'
         self.autor_counter += 1
         return codigo_autor
     
 #------------------AGREGAR_LIBRO--------------------------------------------
 
     def agregar_libro(self, cod_libro, titulo, year, tomo):
-        cod_libro=self.generar_codigo_libro(cod_libro)
+        
         # Crear un diccionario con los datos del libro
         libro_nuevo = {'codigo_libro': cod_libro,
                         'titulo': titulo,
@@ -58,10 +58,6 @@ class Mantenimiento():
     def editar_libro(self, codigo_libro, nuevo_titulo, nuevo_ano, nuevo_tomo):
         bd = BaseDeDatos('base.xlsx')
         return bd.editar_libro(codigo_libro, nuevo_titulo, nuevo_ano, nuevo_tomo)
-
-    def generar_codigo_libro(self):
-        self.codigo_libro = print("genera el codigo del libro")
-        return self.codigo_libro
     #---------------OBTENER LIBROS -------------------------------------------------
 
     def obtener_libros(self):
