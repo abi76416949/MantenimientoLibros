58352be35b1b07f5047611f5e66f8912db2fbf83
Abigail Durand Palacios <127162244+abi76416949@users.noreply.github.com>
2023-09-17 22:13:28 -0500

    añadiendo mantenimiento de autores

--- _libro.py
+++ _libro.py
@@ -46,5 +46,46 @@ class Mantenimiento():
         bd = BaseDeDatos('base.xls')
         return bd.buscar_libro(codigo_libro)
     
-    ################################Autor#####################################
+    ################################AUTOR#####################################
+    def agregar_autor(self, codigo_autor, nombre, apellido):
+        autores_a_guardar = [
+            {'codigo_autor': codigo_autor, 'nombre': nombre, 'apellido': apellido}
+        ]
+
+        bd = BaseDeDatos('base.xls')
+        bd.guardar_autores(autores_a_guardar)
+
+    #.....................ASIGNAR UN AUTOR A LIBRO..............................}
+    def asignar_autor_a_libro(self, codigo_libro, codigo_autor):
+        bd = BaseDeDatos('base.xls')
+        bd.asignar_autor_a_libro(codigo_libro, codigo_autor)
+
+
     
+    
+    #---------------------OBTENER AUTORES----------------------------
+    def obtener_autores(self):
+        bd = BaseDeDatos('base.xls')
+        return bd.obtener_autores()
+    #------------------ELIMINAR AUTORES------------------------------
+    def eliminar_autor(self, codigo_autor):
+        bd = BaseDeDatos('base.xls')
+        return bd.eliminar_autor(codigo_autor)
+    #------------------EDITAR AUTORES------------------------------
+    def editar_autor(self, codigo_autor, nuevo_nombre, nuevo_apellido):
+        bd = BaseDeDatos('base.xls')
+        return bd.editar_autor(codigo_autor, nuevo_nombre, nuevo_apellido)
+
+
+
+##########################CATEGORIAS#####################################################
+
+
+    #------------------Asignar_categoria_a_libro------------------
+    def asignar_categoria_a_libro(self, codigo_libro, codigo_categoria):
+        bd = BaseDeDatos('base.xls')
+        bd.asignar_categoria_a_libro(codigo_libro, codigo_categoria)
+    #---------------------OBTENER CATEGORIAS----------------------------
+    def obtener_categorias(self):
+        bd = BaseDeDatos('base.xls')
+        return bd.obtener_categorias()
